386bc81d30f8e6274e20696af975aaaaec6ed600
Austin Jiang <austin_jiang123@hotmail.com>
2018-10-24 02:07:19 -0400

    oct 24

--- MyBot.py
+++ MyBot.py
@@ -12,19 +12,14 @@ import logging
 
 # This game object contains the initial game state.
 game = hlt.Game()
-# At this point "game" variable is populated with initial map data.
+
 # This is a good place to do computationally expensive start-up pre-processing.
 # As soon as you call "ready" function below, the 2 second per turn timer will start.
 me = game.me
 game_map = game.game_map
-
 shipyard_position = me.shipyard.position
 
 game.ready("holahula")
-
-# Now that your bot is initialized, save a message to yourself in the log file with some important information.
-#   Here, you log here your id, which you can always fetch from the game object by using my_id.
-logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
 
 """ <<<Game Loop>>> """
 ship_status = {}
@@ -37,7 +32,7 @@ while True:
     direction_order = [Direction.North, Direction.South, Direction.East, Direction.West, Direction.Still]
 
     position_choices = [] #physical co-ords on map itself
-
+    
     for ship in me.get_ships():
         #logging.info("Ship {} has {} halite.".format(ship.id, ship.halite_amount))
         if ship.id not in ship_status:
@@ -52,7 +47,6 @@ while True:
 
         # {(0,1):500}
         halite_dict = {} #holds all halite at x, y coordinate
-
         #{(0, 1):200} cost to move to this tile
         cost_dict = {}
 
@@ -63,17 +57,17 @@ while True:
             position = position_dict[direction]
             halite_amount = game_map[position].halite_amount
             if position not in position_choices: #not a tile another ship is moving into
-                halite_dict[direction] = halite_amount #- ship.position.halite_amount / 10
-                #cost_dict[direction] = ship.position.halite_amount / 10
-            else:
-                logging.info("attempting to move to same spot \n")
+                if direction == Direction.Still:   #affects logic for "collecting" - makes it more likely to stay still than to move 
+                    halite_dict[direction] = halite_amount * 2
+                else:
+                    halite_dict[direction] = halite_amount
 
         if ship_status[ship.id] == "depositing":
             if ship.position == me.shipyard.position:
                 ship_status[ship.id] = "collecting"
             else:
                 if game_map[ship.position].halite_amount > (ship.halite_amount / 8) and ship.halite_amount < 800:
-                    move = game_map.naive_navigate(ship, ship.position)
+                    move = Direction.Still
                     position_choices.append(position_dict[move])
                     command_queue.append(ship.move(move))
                 else:
@@ -85,28 +79,37 @@ while True:
             if ship.position == me.shipyard.position:
                 for position in shipyard_options:
                     if game_map[position].is_empty:
-                        command_queue.append(ship.move(game_map.naive_navigate(ship, position)))
+                        move = game_map.naive_navigate(ship, position)
+                        command_queue.append(ship.move(move))
+                        deadlock = False
                         break
+                if deadlock:
+                    command_queue.append(ship.move(Direction.East))
             else:
                 directional_choice = max(halite_dict, key=halite_dict.get)
-                position_choices.append(position_dict[directional_choice])
-                command_queue.append(ship.move(game_map.naive_navigate(ship, position_dict[directional_choice])))
-            
-            if ship.halite_amount > constants.MAX_HALITE / 4: #ship collects to 250 then returns
+                if halite_dict[directional_choice] == 0:
+                    position_choices.append(position_dict[Direction.West])
+                    command_queue.append(ship.move(Direction.West))
+                else:
+                    position_choices.append(position_dict[directional_choice])
+                    command_queue.append(ship.move(game_map.naive_navigate(ship, position_dict[directional_choice])))
+
+            if ship.halite_amount > constants.MAX_HALITE / 2.5: #ship collects to 250 then returns
                 ship_status[ship.id] = "depositing"
 
+        deadlock = True
 
-    if len(me.get_ships()) == 0:
-        command_queue.append(me.shipyard.spawn())
-    elif game.turn_number <= constants.MAX_TURNS / 2:    #first period, prioritize making ships 
-        if me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:# and len(me.get_ships()) < 10:
+    if game.turn_number <= constants.MAX_TURNS *.75 and len(me.get_ships()) < 20:    #first period, prioritize making ships 
+        if me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied and me.shipyard.position not in position_choices:# and len(me.get_ships()) < 10:
             command_queue.append(me.shipyard.spawn())
-    #b/w first third and first half, make ships ONLY if more than 2000 halite and MAX of 5 ships
-    # elif game.turn_number <= (constants.MAX_TURNS / 2) and me.halite_amount > constants.SHIP_COST * 2 and len(me.get_ships()) < 5:
-    #     if me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
-    #         command_queue.append(me.shipyard.spawn())
-    # #third period, make ships ONLY if no ships alive
-
+    # elif me.halite_amount >= constants.SHIP_COST and game_map[me.shipyard].is_occupied and me.shipyard.position not in position_choices:
+    #     command_queue.append(me.shipyard.spawn())
     #END OF TURN    
     game.end_turn(command_queue)
 
+
+# for direction in direction_order:
+#                         if position_dict[direction] not in position_choices and position_dict[direction] != me.shipyard.position:
+#                             position_choices.append(position_dict[direction])
+#                             command_queue.append(ship.move(direction))
+#                             break
